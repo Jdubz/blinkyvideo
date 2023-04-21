367f50fbe4ed867b212ad4d25cbb88bbcfc99e5b
Thomas Johnston <thomas.johnston@opna.dev>
2023-04-20 17:44:05 -0700

    Add simple video webcam stream to video playback app

--- video-feedback/src/App.tsx
+++ video-feedback/src/App.tsx
@@ -1,24 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
+import { useRef, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  const getVideo = () => {
+    return navigator.mediaDevices
+      .getUserMedia({
+        audio: false,
+        video: true,
+      })
+      .then(stream => {
+        const video = videoRef.current;
+        
+        if (video !== null) {
+          video.srcObject = stream;
+          video.play();
+        } else {
+          console.error('video is null');
+        }
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
+
+  useEffect(() => {
+    getVideo();
+  }, [videoRef]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <video 
+        ref={videoRef}
+      />
     </div>
   );
 }
